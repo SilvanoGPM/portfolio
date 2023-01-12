826b42cfc6984e7bcb9c57eb74984f3efa46c67a
SkyG0D <silvanosilvino@hotmail.com>
2023-01-12 12:54:47 -0300

    refactor: pagination-item color

--- src/components/ui/pagination/pagination-item.tsx
+++ src/components/ui/pagination/pagination-item.tsx
@@ -11,31 +11,24 @@ export function PaginationItem({
   number,
   isCurrent = false,
 }: PaginationItemProps) {
-  if (isCurrent) {
-    return (
-      <Button
-        size="sm"
-        fontSize="xs"
-        width="4"
-        colorScheme="brand"
-        disabled
-        color="white"
-        _disabled={{ bg: 'brand.500', cursor: 'default' }}
-      >
-        {number}
-      </Button>
-    );
-  }
-
   return (
     <Button
       onClick={() => onPageChange(number)}
       size="sm"
       fontSize="xs"
       width="4"
-      color="inherit"
-      _dark={{ bg: 'gray.700', _hover: { bg: 'gray.500' } }}
-      _light={{ bg: 'gray.200', _hover: { bg: 'gray.300' } }}
+      disabled={isCurrent}
+      _disabled={{
+        bg: 'brand.100',
+        borderColor: 'brand.100',
+        cursor: 'default',
+      }}
+      _hover={{
+        _disabled: {
+          bg: 'brand.100',
+          borderColor: 'brand.100',
+        },
+      }}
     >
       {number}
     </Button>
